821de70b8a098333d37e8b34abd945724c6deb84
suben <826935261@qq.com>
2025-04-30 08:54:13 +0800

    模型下载优化

--- ui/src/components/bussiness/mgn/model-download.vue
+++ ui/src/components/bussiness/mgn/model-download.vue
@@ -223,7 +223,9 @@ export default {
             line.files.forEach((item, index) => {
               if (item.percent.indexOf('100.00%') < 0) {
                 fetchFluxData('/api/mgn/dl-percent?fileId=' + item.id, (res) => {
-                  item.percent = res
+                  if(res && res.length > 0) {
+                    item.percent = res
+                  }
                 });
               }
             })
