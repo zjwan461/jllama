c07f59bf9436fd3a20291cbd5e2b3c1f55f3d467
jerry.su <826935261@qq.com>
2025-05-02 22:59:24 +0800

    webview多系统适配

---
+++ scripts/startup_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 启动 Java 应用程序
+nohup java -jar -Dspring.config.location=./application.yml --module-path "./javafx-sdk/linux/lib" --add-modules javafx.controls,javafx.web jllama.jar -Dstartbrowser=true > /dev/null 2>&1 &
+echo "Jllama is starting..."
+
+# 等待 5 秒
+sleep 5
+echo "Jllama is started."
